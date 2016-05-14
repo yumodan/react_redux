d052f9771f047172d8a8b40307811883c00c9669
Yusuf Modan <yumodan@gmail.com>
2016-05-14 14:52:20 -0700

    add result test

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,10 +6,18 @@ export default class Results extends React.Component {
   constructor(){
     super()
     this.getPair = this.getPair.bind(this);
+    this.getTally = this.getTally.bind(this);
   }
 
   getPair(){
     return this.props.pair || [];
+  }
+
+  getTally(entry){
+    if(this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
   }
 
   render(){
@@ -18,6 +26,9 @@ export default class Results extends React.Component {
         this.getPair().map(entry => 
           <div key={entry} className="entry">
             <h1>{entry}</h1>
+            <div className="voteCount">
+              {this.getTally(entry)}
+            </div>
           </div>)
       }</div>
     );
---
+++ test/components/Results_spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  renderIntoDocument,
+  scryRenderdDOMComponentsWithClass
+} from 'react-addons-test-utils';
+import {List, Map} from 'immutable';
+import Results from '../../src/components/Results';
+import {expect} from 'chai';
+
+describe('Results', () => {
+  it('renders entries with vote counts or zero', () => {
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const tally = Map({'Trainspotting': 5});
+    const component = renderIntoDocument(
+      <Results pair={pair} tally={tally} />
+    );
+    const entries = scryRenderdDOMComponentsWithClass(component, 'entry');
+    const [train, days] = entries.map(element => element.textContent);
+
+    expect(entries.length).to.equal(2);
+    expect(train).to.contain('Trainspotting');
+    expect(train).to.contain('5');
+    expect(days).to.contain('28 Days Later');
+    expect(days).to.contain('0');
+  });
+  
+});
